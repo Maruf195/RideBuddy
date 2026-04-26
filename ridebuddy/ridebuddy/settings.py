e437b28b94fd1acbbf812f94f76d7160baa721ac
Maruf195 <129549328+Maruf195@users.noreply.github.com>
2026-04-27 01:04:17 +0600

    fixed update

--- ridebuddy/ridebuddy/settings.py
+++ ridebuddy/ridebuddy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)jzwv--*n-2a3&-4*&x0id9p4pj#(b5$!8z1(a&^jd--fuwhs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'False'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
